b8c15ef4cb1a0447d2bace13bccb370958265f23
Kei Murakami <buncho514@icloud.com>
2021-04-05 13:25:14 +0900

    ignore修正

--- categorizePractice/src/FaceDetect.py
+++ categorizePractice/src/FaceDetect.py
@@ -10,16 +10,17 @@ X_test = []
 Y_test = []
 
 l = os.listdir("categorizePractice/myDataSets/inputFile")
+print(l)
 for i in l:
     result_dir = i.split(".")[0]
-    os.mkdir("categorizePractice/myDataSets/inputFile/" + result_dir)
+    os.mkdir("categorizePractice/myDataSets/martial/" + result_dir)
     m = os.listdir("categorizePractice/myDataSets/inputFile/" + i)
     n = 0
     for target_file in m:
-        origin_image = ("categorizePractice/myDataSets/inputFile" + i + "/" + target_file)
+        origin_image = ("categorizePractice/myDataSets/inputFile/" + i + "/" + target_file)
+        print("origin_image",origin_image)
         cascade = cv2.CascadeClassifier(cascade_path)
         image = cv2.imread(origin_image)
-
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
@@ -28,18 +29,18 @@ for i in l:
         for x, y, w, h in facerect:
             face = gray[y:y + h, x:x + w]
             face = cv2.resize(face, (128, 128))
-            save_path = "categorizePractice/myDataSets/martial" + result_dir + "/image_" + str(n) + ".jpg"
+            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
             cv2.imwrite(save_path, face)
             n = n + 1
             face = cv2.flip(face, 1)
-            save_path = "categorizePractice/myDataSets/martial" + result_dir + "/image_" + str(n) + ".jpg"
+            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
             cv2.imwrite(save_path, face)
             n = n + 1
             face = cv2.blur(face, (10, 10))
-            save_path = "categorizePractice/myDataSets/martial" + result_dir + "/image_" + str(n) + ".jpg"
+            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
             cv2.imwrite(save_path, face)
             n = n + 1
             face = cv2.blur(cv2.flip(face, 1), (10, 10))
-            save_path = "categorizePractice/myDataSets/martial" + result_dir + "/image_" + str(n) + ".jpg"
+            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
             cv2.imwrite(save_path, face)
             n = n + 1
