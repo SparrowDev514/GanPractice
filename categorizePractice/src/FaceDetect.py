1513d6715ab947310265136db2ff7b89111f8df8
Kei Murakami <buncho514@icloud.com>
2021-04-05 14:48:19 +0900

    空ファイルを追加したい

--- categorizePractice/src/FaceDetect.py
+++ categorizePractice/src/FaceDetect.py
@@ -12,34 +12,41 @@ Y_test = []
 l = os.listdir("categorizePractice/myDataSets/inputFile")
 for i in l:
     result_dir = i.split(".")[0]
-    os.mkdir("categorizePractice/myDataSets/martial/" + result_dir)
-    m = os.listdir("categorizePractice/myDataSets/inputFile/" + i)
-    n = 0
-    for target_file in m:
-        origin_image = ("categorizePractice/myDataSets/inputFile/" + i + "/" + target_file)
-        print("origin_image",origin_image)
-        cascade = cv2.CascadeClassifier(cascade_path)
-        image = cv2.imread(origin_image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-        facerect = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(10, 10))
+    if result_dir == "":
+        print("this is config file")
+    else:
+        print(result_dir)
+        os.mkdir("categorizePractice/myDataSets/martial/" + result_dir)
+        m = os.listdir("categorizePractice/myDataSets/inputFile/" + i)
+        n = 0
+        for target_file in m:
+            origin_image = ("categorizePractice/myDataSets/inputFile/" + i + "/" + target_file)
+            if origin_image.split(".")[1] == "gitignore":
+            else:
+                print("origin_image",origin_image)
+                cascade = cv2.CascadeClassifier(cascade_path)
+                image = cv2.imread(origin_image)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-        for x, y, w, h in facerect:
-            face = gray[y:y + h, x:x + w]
-            face = cv2.resize(face, (128, 128))
-            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
-            cv2.imwrite(save_path, face)
-            n = n + 1
-            face = cv2.flip(face, 1)
-            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
-            cv2.imwrite(save_path, face)
-            n = n + 1
-            face = cv2.blur(face, (10, 10))
-            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
-            cv2.imwrite(save_path, face)
-            n = n + 1
-            face = cv2.blur(cv2.flip(face, 1), (10, 10))
-            save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
-            cv2.imwrite(save_path, face)
-            n = n + 1
+                facerect = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(10, 10))
+
+                for x, y, w, h in facerect:
+                    face = gray[y:y + h, x:x + w]
+                    face = cv2.resize(face, (128, 128))
+                    save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
+                    cv2.imwrite(save_path, face)
+                    n = n + 1
+                    face = cv2.flip(face, 1)
+                    save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
+                    cv2.imwrite(save_path, face)
+                    n = n + 1
+                    face = cv2.blur(face, (10, 10))
+                    save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
+                    cv2.imwrite(save_path, face)
+                    n = n + 1
+                    face = cv2.blur(cv2.flip(face, 1), (10, 10))
+                    save_path = "categorizePractice/myDataSets/martial/" + result_dir + "/image_" + str(n) + ".jpg"
+                    cv2.imwrite(save_path, face)
+                    n = n + 1
